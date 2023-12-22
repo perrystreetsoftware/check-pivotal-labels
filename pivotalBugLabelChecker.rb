2da6f4e6c200d727d577856d03da01d148873413
Bruno Campos <bruno@perrystreet.com>
2023-12-21 23:32:35 -0300

    Update pivotalBugLabelChecker.rb

--- pivotalBugLabelChecker.rb
+++ pivotalBugLabelChecker.rb
@@ -1,4 +1,4 @@
-branch_name = ENV['GITHUB_REF']  # GITHUB_REF contains the branch reference .split('/').last
+branch_name = ENV['GITHUB_HEAD_REF']  # GITHUB_REF contains the branch reference .split('/').last
 puts #{branch_name}
 branch_id_match = branch_name.match(/\[#(\d+)\]/)  # Extract branch ID within square brackets
 branch_id = branch_id_match[1] if branch_id_match
