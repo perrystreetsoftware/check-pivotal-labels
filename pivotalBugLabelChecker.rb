3c578f2a2c761eb78a4a939fff9c0ad88e0fbdde
Bruno Campos <bruno@perrystreet.com>
2023-12-21 23:35:01 -0300

    Update pivotalBugLabelChecker.rb

--- pivotalBugLabelChecker.rb
+++ pivotalBugLabelChecker.rb
@@ -1,10 +1,9 @@
 branch_name = ENV['GITHUB_HEAD_REF']  # GITHUB_REF contains the branch reference .split('/').last
-puts #{branch_name}
-branch_id_match = branch_name.match(/\[#(\d+)\]/)  # Extract branch ID within square brackets
+branch_id_match = branch_name.match(/_(\d+)$/)  # Extract numeric ID at the end of the branch name
 branch_id = branch_id_match[1] if branch_id_match
 
 unless branch_id
-  raise "Unable to extract branch ID from the branch name '#{branch_name}'."
+  raise "Unable to extract ID from the branch name '#{branch_name}'."
 end
 
-puts "Branch ID: #{branch_id}"
+puts "ID: #{branch_id}"
