87d65833c1831d8487b7641decb710739f1b192d
Bruno Campos <bruno@perrystreet.com>
2023-12-21 23:48:41 -0300

    Update pivotalBugLabelChecker.rb

--- pivotalBugLabelChecker.rb
+++ pivotalBugLabelChecker.rb
@@ -1,16 +1,12 @@
 require 'tracker_api'
 
-branch_name = ENV['GITHUB_HEAD_REF']  # GITHUB_REF contains the branch reference .split('/').last
+branch_name = ENV['GITHUB_HEAD_REF'] 
 branch_id_match = branch_name.match(/_(\d+)$/)  # Extract numeric ID at the end of the branch name
 branch_id = branch_id_match[1] if branch_id_match
 
 unless branch_id
   raise "Unable to extract ID from the branch name '#{branch_name}'."
 end
-
-puts "ID: #{branch_id}"
-
-
 
 # Set your Pivotal Tracker API token and project ID
 token = ENV['PIVOTAL_TOKEN']
